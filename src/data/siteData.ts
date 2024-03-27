08c0aa246d4c304ce00336cab3d610757ec6f447
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-03-27 19:34:53 +0300

    feat(app): Add nav bar

---
+++ src/data/siteData.ts
@@ -0,0 +1,7 @@
+export const baseData = {
+  title: "Stephen Kiiyuru Portfolio Site",
+  description: `Welcome to my portfolio site!
+   I'm a Front End Engineer obsessed
+   with immersive experiences.
+  `,
+}
