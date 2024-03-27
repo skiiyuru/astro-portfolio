522f52a88c944390f77d69b92f15b4f528293835
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-03-27 22:59:27 +0300

    feat(app): Add footer

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -5,3 +5,21 @@ export const baseData = {
    with immersive experiences.
   `,
 }
+
+export const socialData = [
+  {
+    href: "https://github.com/skiiyuru",
+    icon: "github",
+    title: "Github",
+  },
+  {
+    href: "https://www.linkedin.com/in/skiiyuru/",
+    icon: "linkedin",
+    title: "Linked In",
+  },
+  {
+    href: "https://twitter.com/snkiiyuru",
+    icon: "twitter",
+    title: "Twitter",
+  },
+]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,9 +6,20 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
         display: ["Pixelify Sans Variable", ...defaultTheme.fontFamily.sans],
+        // sans: ["Open Sans Variable", ...defaultTheme.fontFamily.sans],
       },
       // colors: {
       //   black: "#090909",
