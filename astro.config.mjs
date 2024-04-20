42e3f9e45dba0b20d54eae7930f176666cfcadbf
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-04-20 23:57:50 +0300

    build(app): update live site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://stargazers.club",
+  site: "https://skiiyuru.netlify.app/",
   prefetch: {
     prefetchAll: true,
   },
