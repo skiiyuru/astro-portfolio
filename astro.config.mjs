a64eea4a1dc6371649207d4130971dc3d0f270c1
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-04-20 23:32:56 +0300

    feat(app): Add prefetch and sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
-
 import netlify from "@astrojs/netlify"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://stargazers.club",
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [tailwind(), icon(), sitemap()],
   output: "hybrid",
   adapter: netlify(),
 })
